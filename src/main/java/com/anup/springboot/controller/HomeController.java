aec3e12ca8a850912ab56bd10b9a115a5c4b5b62
saikrishna2653 <37019838+saikrishna2653@users.noreply.github.com>
2020-03-05 13:38:47 +0530

    ss

--- src/main/java/com/anup/springboot/controller/HomeController.java
+++ src/main/java/com/anup/springboot/controller/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	
-	@RequestMapping(value="/", method =RequestMethod.GET)
+	@RequestMapping(value="/home", method =RequestMethod.GET)
 	public String getHomePage() {
 		
 		
